2f3d27f130df03d122e808055dacaeb2afd6d8b3
michead <countrymetal@live.it>
2015-04-11 23:36:11 +0200

    fragments in backstack

--- app/src/main/java/com/spaceapps/meatanagram/spaceappsproject/PostFragment.java
+++ app/src/main/java/com/spaceapps/meatanagram/spaceappsproject/PostFragment.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +14,7 @@ import android.widget.TextView;
 /**
  * Created by Simone on 4/11/2015.
  */
-public class PostFragment extends Fragment implements View.OnKeyListener {
+public class PostFragment extends Fragment {
 
     private View view;
 
@@ -22,23 +23,27 @@ public class PostFragment extends Fragment implements View.OnKeyListener {
         super.onCreateView(inflater, container, savedInstanceState);
 
         view = inflater.inflate(R.layout.post_layout, container, false);
+
         TextView tv = (TextView)view.findViewById(R.id.textView1);
         int position = getArguments().getInt("position");
         tv.setText("Post at position " + position + ".");
 
+        view.setFocusableInTouchMode(true);
+        view.requestFocus();
+        view.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_BACK) {
+                    getFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        });
+
         return view;
-    }
-
-    @Override
-    public boolean onKey( View v, int keyCode, KeyEvent event )
-    {
-        if( keyCode == KeyEvent.KEYCODE_BACK )
-        {
-            ((MainActivity)getActivity()).showTiles();
-
-            return true;
-        }
-
-        return false;
     }
 }
