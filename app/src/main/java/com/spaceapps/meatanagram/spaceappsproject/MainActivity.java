561d8b0fc90f45df8de9d66ae9f735a64a824943
michead <countrymetal@live.it>
2015-04-11 22:54:17 +0200

    added fragments

--- app/src/main/java/com/spaceapps/meatanagram/spaceappsproject/MainActivity.java
+++ app/src/main/java/com/spaceapps/meatanagram/spaceappsproject/MainActivity.java
@@ -1,8 +1,10 @@
 package com.spaceapps.meatanagram.spaceappsproject;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,38 +13,8 @@ import com.parse.ParseFacebookUtils;
 import com.parse.ui.ParseLoginActivity;
 import com.parse.ui.ParseLoginBuilder;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
-    private StaggeredGridView sgv;
-    private int margin = 10;
-    private String urls[] = {
-            "http://farm7.staticflickr.com/6101/6853156632_6374976d38_c.jpg",
-            "http://farm8.staticflickr.com/7232/6913504132_a0fce67a0e_c.jpg",
-            "http://farm5.staticflickr.com/4133/5096108108_df62764fcc_b.jpg",
-            "http://farm5.staticflickr.com/4074/4789681330_2e30dfcacb_b.jpg",
-            "http://farm9.staticflickr.com/8208/8219397252_a04e2184b2.jpg",
-            "http://farm9.staticflickr.com/8483/8218023445_02037c8fda.jpg",
-            "http://farm9.staticflickr.com/8335/8144074340_38a4c622ab.jpg",
-            "http://farm9.staticflickr.com/8060/8173387478_a117990661.jpg",
-            "http://farm9.staticflickr.com/8056/8144042175_28c3564cd3.jpg",
-            "http://farm9.staticflickr.com/8183/8088373701_c9281fc202.jpg",
-            "http://farm9.staticflickr.com/8185/8081514424_270630b7a5.jpg",
-            "http://farm9.staticflickr.com/8462/8005636463_0cb4ea6be2.jpg",
-            "http://farm9.staticflickr.com/8306/7987149886_6535bf7055.jpg",
-            "http://farm9.staticflickr.com/8444/7947923460_18ffdce3a5.jpg",
-            "http://farm9.staticflickr.com/8182/7941954368_3c88ba4a28.jpg",
-            "http://farm9.staticflickr.com/8304/7832284992_244762c43d.jpg",
-            "http://farm9.staticflickr.com/8163/7709112696_3c7149a90a.jpg",
-            "http://farm8.staticflickr.com/7127/7675112872_e92b1dbe35.jpg",
-            "http://farm8.staticflickr.com/7111/7429651528_a23ebb0b8c.jpg",
-            "http://farm9.staticflickr.com/8288/7525381378_aa2917fa0e.jpg",
-            "http://farm6.staticflickr.com/5336/7384863678_5ef87814fe.jpg",
-            "http://farm8.staticflickr.com/7102/7179457127_36e1cbaab7.jpg",
-            "http://farm8.staticflickr.com/7086/7238812536_1334d78c05.jpg",
-            "http://farm8.staticflickr.com/7243/7193236466_33a37765a4.jpg",
-            "http://farm8.staticflickr.com/7251/7059629417_e0e96a4c46.jpg",
-            "http://farm8.staticflickr.com/7084/6885444694_6272874cfc.jpg"
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +35,7 @@ public class MainActivity extends Activity {
             startActivityForResult(loginIntent, R.integer.login_request_code);
         }
 
+        /*
         sgv = (StaggeredGridView)findViewById(R.id.staggeredGridView1);
         sgv.setItemMargin(margin); // set the GridView margin
         sgv.setPadding(margin, 0, margin, 0); // have the margin on the sides as well
@@ -71,6 +44,9 @@ public class MainActivity extends Activity {
 
         sgv.setAdapter(adapter);
         adapter.notifyDataSetChanged();
+        */
+
+        this.getSupportFragmentManager().beginTransaction().replace(R.id.frag_holder, new MainFragment()).commit();
     }
 
 
---
+++ app/src/main/java/com/spaceapps/meatanagram/spaceappsproject/PostFragment.java
@@ -0,0 +1,9 @@
+package com.spaceapps.meatanagram.spaceappsproject;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Simone on 4/11/2015.
+ */
+public class PostFragment extends Fragment {
+}
